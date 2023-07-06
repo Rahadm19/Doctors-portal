5f0c7dcea23238a9f9b72eb791a849d9a722e54c
Rahadm19 <rahadm19@gmail.com>
2023-07-06 16:06:23 +0600

    justify content

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,11 +3,11 @@ import chair from '../../../assets/images/chair.png'
 
 const Banner = () => {
     return (
-        <div>
-            <div className="hero bg-image">
+        <div className="bg-image">
+            <div className="hero">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
+                    <img src={chair} className="max-w-sm rounded-lg shadow-2xl basis-1/2" />
+                    <div className="basis-1/2">
                         <h1 className="text-5xl font-bold">Box Office News!</h1>
                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                         <button className="btn btn-primary">Get Started</button>
